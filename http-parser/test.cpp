08862e239ed6273167c4b37eb624c821fa8b6d27
Long Sun <iterator_sl@sina.com>
2017-03-02 22:58:02 +0800

    project init

---
+++ http-parser/test.cpp
@@ -0,0 +1,24 @@
+#include "http_parser.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef struct HTTP_REQUEST_ {
+
+}HttpRequest;
+
+
+typedef struct HTTP_RESPONSE_ {
+
+}HttpResponse;
+
+int main() {
+
+	http_parser_settings settings;
+	
+	http_parser parser;
+
+
+	return 0;
+}
